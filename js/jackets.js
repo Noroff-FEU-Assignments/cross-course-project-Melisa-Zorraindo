258fb456f58c5770d8744dfbed5a8177cedd0c4b
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-03-08 14:10:55 +0100

    add js to render jacket list

---
+++ js/jackets.js
@@ -0,0 +1,294 @@
+const jacketList = [
+  {
+    name: "Aconcagua",
+    type: "Rain and windshield jacket",
+    price: 259,
+    image: "../images/jackets/aconcagua.jpg",
+    description:
+      "Warm and lightweight jacket especially built for rafting and water sports with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Water resistant zippers",
+    sports: ["Rafting", "Rowing", "Hiking", "Trekking", "Climbing"],
+    id: "000",
+  },
+
+  {
+    name: "Alaska",
+    type: "Gore-Tex insulated jacket",
+    price: 399,
+    image: "../images/jackets/alaska.jpg",
+    description:
+      "Warm and lightweight jacket especially built for hiking and moderate to vigorous outdoor activities with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Water and wind resistant for increased protection.",
+    sports: ["Hiking", "Cross-Country Skiing", "Snowboarding"],
+    id: "001",
+  },
+
+  {
+    name: "Aukland",
+    type: "Extra light rain jacket",
+    price: 349,
+    image: "../images/jackets/aukland.jpg",
+    description:
+      "Extra lightweight jacket especially built for light to moderate to outdoor activities with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Wind resistant for increased protection.",
+    sports: ["Hiking", "Trekking"],
+    id: "002",
+  },
+
+  {
+    name: "Baikal",
+    type: "Extra light rain jacket",
+    price: 399,
+    image: "../images/jackets/baikal.jpg",
+    description:
+      "Extra lightweight jacket especially built for light to moderate to outdoor activities with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Wind resistant for increased protection.",
+    sports: ["Hiking", "Trekking"],
+    id: "003",
+  },
+
+  {
+    name: "Chapala",
+    type: "Extra lightweighted winter jacket",
+    price: 399,
+    image: "../images/jackets/chapala.jpg",
+    description:
+      "Extra lightweight jacket especially built for light outdoor activities with a modern and stylish design. It offers plenty of storage space with hidden side pockets and two regular side pockets. Wind and water resistant for increased protection. Invisible zippers and velcro cuff fasteners.",
+    sports: ["Hiking", "Trekking"],
+    id: "016",
+  },
+
+  {
+    name: "Daisen",
+    type: "Parka",
+    price: 399,
+    image: "../images/jackets/daisen.jpg",
+    description:
+      "Warm and lightweighted parka especially built for light outdoor activities like hiking and trekking. It has a smart and elegant desing with fleece on the inside and keep-dry technology. Hidden pockets inside and two side pockets outside. Water and wind resistant for increased protection.",
+    sports: ["Hiking", "Trekking"],
+    id: "004",
+  },
+
+  {
+    name: "Emei Shan",
+    type: "Parka",
+    price: 549,
+    image: "../images/jackets/emei-shan.jpg",
+    description:
+      "Warm and lightweighted parka especially built for light outdoor activities like hiking and trekking. It has a smart and elegant desing with fleece on the inside and keep-dry technology. Hidden pockets inside and two side pockets outside. Water and wind resistant for increased protection.",
+    sports: ["Hiking", "Trekking"],
+    id: "005",
+  },
+
+  {
+    name: "Erie",
+    type: "Extra lightweighted winter jacket",
+    price: 549,
+    image: "../images/jackets/erie.jpg",
+    description:
+      "Extra lightweight jacket especially built for light outdoor activities with a modern and stylish design. It offers plenty of storage space with hidden side pockets and two regular side pockets. Wind and water resistant for increased protection. Invisible zippers and velcro cuff fasteners.",
+    sports: ["Hiking"],
+    id: "010",
+  },
+
+  {
+    name: "Everest",
+    type: "Rain and windshield jacket",
+    price: 399,
+    image: "../images/jackets/everest.jpg",
+    description:
+      "Warm and lightweight jacket especially built for rafting and water sports with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Water resistant zippers",
+    sports: ["Rafting", "Rowing", "Hiking", "Trekking", "Climbing"],
+    id: "006",
+  },
+
+  {
+    name: "Gaisberg",
+    type: "Extra light rain jacket",
+    price: 249,
+    image: "../images/jackets/gaisberg.jpg",
+    description:
+      "Extra lightweight jacket especially built for light to moderate to outdoor activities with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Wind resistant for increased protection.",
+    sports: ["Hiking", "Trekking"],
+    id: "007",
+  },
+
+  {
+    name: "Helsinki",
+    type: "Extra lightweighted winter jacket",
+    price: 449,
+    image: "../images/jackets/helsinki.jpg",
+    description:
+      "Extra lightweight jacket especially built for light outdoor activities with a modern and stylish design. It offers plenty of storage space with hidden side pockets and two regular side pockets. Wind and water resistant for increased protection. Invisible zippers and velcro cuff fasteners.",
+    sports: ["Hiking"],
+    id: "008",
+  },
+
+  {
+    name: "Himalaya",
+    type: "Gore-Tex insulated jacket",
+    price: 379,
+    image: "../images/jackets/himalaya.jpg",
+    description:
+      "Warm and lightweight jacket especially built for hiking and moderate to vigorous outdoor activities with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Water and wind resistant for increased protection.",
+    sports: ["Hiking", "Cross-Country Skiing", "Snowboarding"],
+    id: "009",
+  },
+
+  {
+    name: "Kauai",
+    type: "Rain and wind shield jacket",
+    price: 389,
+    image: "../images/jackets/kauai.jpg",
+    description:
+      "Warm and lightweight jacket especially built for rafting and water sports with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Water resistant zippers",
+    sports: ["Rafting", "Rowing", "Hiking", "Trekking", "Climbing"],
+    id: "011",
+  },
+
+  {
+    name: "Maui",
+    type: "Insulated down coat",
+    price: 379,
+    image: "../images/jackets/maui.jpg",
+    description:
+      "Warm, insulated down coat especially built for light to moderate outdoor activities. It has a modern and elegant design that combines our keep-dry technology with the comfort of the most environmentaly friendly fabrics. Wind and water resistant for increased protection. Hidden hoodie.",
+    sports: ["Hiking", "Trekking", "Cross-Country skiing", "Fishing"],
+    id: "013",
+  },
+
+  {
+    name: "Mendoza",
+    type: "Rain coat",
+    price: 249,
+    image: "../images/jackets/mendoza.jpg",
+    description:
+      "Warm and lightweight raincoat especially built for water sports like rafting and rowing. It has a modern and elegant design that combines our keep-dry technology with the comfort of ultra lightweighted tyvek. Wind resistant for increased protection. Water resistant zippers.",
+    sports: ["Hiking", "Trekking", "Rowing", "Rafting", "Fishing"],
+    id: "014",
+  },
+
+  {
+    name: "Noshaq",
+    type: "Rain coat",
+    price: 299,
+    image: "../images/jackets/noshaq.jpg",
+    description:
+      "Warm and lightweight raincoat especially built for water sports like rafting and rowing. It has a modern and elegant design that combines our keep-dry technology with the comfort of ultra lightweighted tyvek. Wind resistant for increased protection. Water resistant zippers.",
+    sports: ["Hiking", "Trekking", "Rowing", "Rafting", "Fishing"],
+    id: "015",
+  },
+
+  {
+    name: "Rysy",
+    type: "Gore-Tex insulated jacket",
+    price: 459,
+    image: "../images/jackets/rysy.jpg",
+    description:
+      "Warm and lightweight jacket especially built for hiking and moderate to vigorous outdoor activities with a modern and stylish design. It offers a lot of storage space with hidden side pockets and two regular side pockets. Water and wind resistant for increased protection.",
+    sports: ["Hiking", "Cross-Country Skiing", "Snowboarding"],
+    id: "017",
+  },
+
+  {
+    name: "Salcantay",
+    type: "Extra lightweighted winter jacket",
+    price: 649,
+    image: "../images/jackets/salcantay.jpg",
+    description:
+      "Extra lightweight jacket especially built for light outdoor activities with a modern and stylish design. It offers plenty of storage space with hidden side pockets and two regular side pockets. Wind and water resistant for increased protection. Invisible zippers and velcro cuff fasteners.",
+    sports: ["Hiking"],
+    id: "018",
+  },
+
+  {
+    name: "Sandersee",
+    type: "Rain coat",
+    price: 279,
+    image: "../images/jackets/sandersee.jpg",
+    description:
+      "Warm and lightweight raincoat especially built for water sports like rafting and rowing. It has a modern and elegant design that combines our keep-dry technology with the comfort of ultra lightweighted tyvek. Wind resistant for increased protection. Water resistant zippers.",
+    sports: ["Hiking", "Trekking", "Rowing", "Rafting", "Fishing"],
+    id: "012",
+  },
+
+  {
+    name: "Shengena",
+    type: "Insulated down coat",
+    price: 349,
+    image: "../images/jackets/shengena.jpg",
+    description:
+      "Warm, insulated down coat especially built for light to moderate outdoor activities. It has a modern and elegant design that combines our keep-dry technology with the comfort of the most environmentaly friendly fabrics. Wind and water resistant for increased protection. Hidden hoodie.",
+    sports: ["Hiking", "Trekking", "Cross-Country skiing", "Fishing"],
+    id: "019",
+  },
+
+  {
+    name: "Tavan Bogd",
+    type: "Insulated down coat",
+    price: 489,
+    image: "../images/jackets/tavan-bogd.jpg",
+    description:
+      "Warm, insulated down coat especially built for light to moderate outdoor activities. It has a modern and elegant design that combines our keep-dry technology with the comfort of the most environmentaly friendly fabrics. Wind and water resistant for increased protection. Hidden hoodie.",
+    sports: ["Hiking", "Trekking", "Cross-Country skiing", "Fishing"],
+    id: "020",
+  },
+
+  {
+    name: "Tutendo",
+    type: "Rain coat",
+    price: 199,
+    image: "../images/jackets/tutendo.jpg",
+    description:
+      "Warm and lightweight raincoat especially built for water sports like rafting and rowing. It has a modern and elegant design that combines our keep-dry technology with the comfort of ultra lightweighted tyvek. Wind resistant for increased protection. Water resistant zippers.",
+    sports: ["Hiking", "Trekking", "Rowing", "Rafting", "Fishing"],
+    id: "021",
+  },
+
+  {
+    name: "Vancouver",
+    type: "Parka",
+    price: 759,
+    image: "../images/jackets/vancouver.jpg",
+    description:
+      "Warm and lightweighted parka especially built for light outdoor activities like hiking and trekking. It has a smart and elegant desing with fleece on the inside and keep-dry technology. Hidden pockets inside and two side pockets outside. Water and wind resistant for increased protection.",
+    sports: ["Hiking", "Trekking"],
+    id: "022",
+  },
+];
+
+//select elements in the dom
+let divContainer = document.querySelector(".items-wrapper");
+
+//function to create HTML
+function createHTML(jacketList) {
+  //set new page heading
+  divContainer.innerHTML = `<h1>Comes from a variable, think about it</h1>`;
+
+  //create rest f HTML
+  jacketList.forEach((jacket) => {
+    divContainer.innerHTML += `<div class="items">
+                                <div>
+                                    <a href="../jackets/alaska.html?=id${jacket.id}"
+                                    ><img
+                                        alt="${jacket.type}"
+                                        class="jacket-image"
+                                        src="${jacket.image}"
+                                    /></a>
+                                </div>
+                                <div class="jacket-info">
+                                    <h2>
+                                    <a href="../jackets/alaska.html?=id${jacket.id}">${jacket.name}</a>
+                                    </h2>
+                                    <p>${jacket.type}</p>
+                                    <p>&dollar; ${jacket.price}</p>
+                                    <div class="interactions">
+                                        <div>
+                                            <i class="fas fa-heart"></i>
+                                        </div>
+                                        <div>
+                                        <a href="../jackets/alaska.html?=id${jacket.id}" class="cta cta-small">Buy</a>
+                                        </div>
+                                    </div>
+                                </div>
+                               </div>`;
+  });
+}
+
+createHTML(jacketList);
