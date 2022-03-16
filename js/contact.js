3978c4616f98890cf2ab361e2d75c6dbcbe4b101
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-03-16 18:14:01 +0100

    added contact form and validation

---
+++ js/contact.js
@@ -0,0 +1,64 @@
+const form = document.querySelector(".contact-form");
+
+const firstName = document.querySelector("#name");
+const errorName = document.querySelector("#error-name");
+
+const subject = document.querySelector("#subject");
+const errorSubject = document.querySelector("#error-subject");
+
+const email = document.querySelector("#email");
+const errorEmail = document.querySelector("#error-email");
+
+const address = document.querySelector("#address");
+const errorAddress = document.querySelector("#error-address");
+
+const successMessage = document.querySelector(".success-message");
+const instructionsForm = document.querySelector(".instructions-para");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (checkLengths(firstName.value, 1)) {
+    errorName.style.display = "block";
+    return false;
+  }
+
+  if (checkLengths(subject.value, 10)) {
+    errorSubject.style.display = "block";
+    return false;
+  }
+
+  if (!checkEmail(email.value)) {
+    errorEmail.style.display = "block";
+    return false;
+  }
+
+  if (checkLengths(address.value, 25)) {
+    errorAddress.style.display = "block";
+    return false;
+  }
+
+  if (checkLengths && checkEmail) {
+    successMessage.style.display = "block";
+    instructionsForm.style.display = "none";
+    errorName.style.display = "none";
+    errorSubject.style.display = "none";
+    errorEmail.style.display = "none";
+    errorAddress.style.display = "none";
+  }
+  form.reset();
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLengths(val, len) {
+  if (val.trim().length < len) {
+    return true;
+  }
+}
+
+function checkEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const regExMatches = regEx.test(email);
+  return regExMatches;
+}
