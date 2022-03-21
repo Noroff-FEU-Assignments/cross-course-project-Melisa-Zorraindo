2428754b4b2bf46a85000156290b64ee0be814a1
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-03-21 13:58:04 +0100

    update contact form

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,7 @@
 const form = document.querySelector(".contact-form");
+const submitButton = document.querySelector(".cta");
+submitButton.disabled = true;
+submitButton.classList.add("disabled");
 
 const firstName = document.querySelector("#name");
 const errorName = document.querySelector("#error-name");
@@ -12,31 +15,46 @@ const errorEmail = document.querySelector("#error-email");
 const successMessage = document.querySelector(".success-message");
 const instructionsForm = document.querySelector(".instructions-para");
 
-function validateForm(event) {
-  event.preventDefault();
-
-  if (
-    checkLengths(firstName.value, 1) ||
-    checkLengths(subject.value, 10) ||
-    !checkEmail(email.value)
-  ) {
+function validateForm() {
+  if (checkLengths(firstName.value, 1)) {
     errorName.style.display = "block";
+    return false;
+  } else {
+    errorName.style.display = "none";
+  }
+
+  if (checkLengths(subject.value, 5)) {
     errorSubject.style.display = "block";
+    return false;
+  } else {
+    errorSubject.style.display = "none";
+  }
+
+  if (!checkEmail(email.value)) {
     errorEmail.style.display = "block";
     return false;
+  } else {
+    errorEmail.style.display = "none";
   }
 
   if (checkLengths && checkEmail) {
-    successMessage.style.display = "block";
-    instructionsForm.style.display = "none";
-    errorName.style.display = "none";
-    errorSubject.style.display = "none";
-    errorEmail.style.display = "none";
+    submitButton.disabled = false;
+    submitButton.classList.remove("disabled");
   }
+}
+
+firstName.addEventListener("keyup", validateForm);
+subject.addEventListener("keyup", validateForm);
+email.addEventListener("keyup", validateForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  successMessage.style.display = "block";
+  instructionsForm.style.display = "none";
   form.reset();
 }
 
-form.addEventListener("submit", validateForm);
+form.addEventListener("submit", submitForm);
 
 function checkLengths(val, len) {
   if (val.trim().length < len) {
